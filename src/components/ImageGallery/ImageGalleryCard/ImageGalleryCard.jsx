9feaf1c9e1f1d2111044864b84df8aa19764ce49
Juliya <BoikoJuliya2707@gmail.com>
2022-07-15 15:54:59 +0300

    galleryHooks

--- src/components/ImageGallery/ImageGalleryCard/ImageGalleryCard.jsx
+++ src/components/ImageGallery/ImageGalleryCard/ImageGalleryCard.jsx
@@ -1,29 +1,29 @@
-import { Component } from "react";
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { Modal } from '../../common/Modal/Modal';
 import { ImageGalleryCardImage } from './ImageGalleryCard.styled';
 
-export class ImageGalleryCard extends Component {
-    state = {
-        isModalOpen: false,
-    } 
+export const ImageGalleryCard = ({ item }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    static propTypes = {
-        item: PropTypes.object.isRequired,
-    };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <div>
+      <ImageGalleryCardImage
+        src={item.webformatURL}
+        onClick={openModal}
+        alt=""
+      />
+      {isModalOpen && <Modal src={item.largeImageURL} onClose={closeModal} />}
+    </div>
+  );
+};
 
-    openModal = () => {this.setState({ isModalOpen: true, })}
-
-    closeModal = () => {this.setState({ isModalOpen: false, })}
-
-    render() {
-        const { item } = this.props;
-        const { isModalOpen } = this.state;
-        return (
-            <div>
-                <ImageGalleryCardImage src={item.webformatURL} onClick={this.openModal} alt="" />
-                {isModalOpen && <Modal src={item.largeImageURL} onClose={this.closeModal} />}
-            </div>
-        );
-    }
-}
+ImageGalleryCard.propTypes = {
+  item: PropTypes.object.isRequired,
+};
